4339e77ac08e3fb029127fd46fb705a89c748b47
s-fukuzawaa <airei.fukuzawa@gmail.com>
2022-12-23 15:05:10 -0800

    [Pages] Finished login register, added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/home/Home"
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single"
 import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 function App() {
   return (
     <>
       <TopBar/>
-      <Settings />
+      <Register />
     </>
     
   );
